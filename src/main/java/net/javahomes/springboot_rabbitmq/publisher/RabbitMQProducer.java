3816100186d96594891fad16e56b227888bae551
Sonal Tamse <sonal.tamse@gmail.com>
2026-07-19 14:18:56 +0530

    create producer

---
+++ src/main/java/net/javahomes/springboot_rabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,28 @@
+package net.javahomes.springboot_rabbitmq.publisher;
+
+import org.slf4j.Logger;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message){
+        logger.info("Sending Message: {}", message);
+        rabbitTemplate.convertAndSend(exchange,routingKey,message);
+    }
+}
