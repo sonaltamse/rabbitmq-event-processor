2507f3495d41ce7bfde5ffd7b067cbfb3474d5e1
Sonal Tamse <sonal.tamse@gmail.com>
2026-07-19 14:00:06 +0530

    configure project to use RabbitMQ

---
+++ src/test/java/net/javahomes/springboot_rabbitmq/SpringbootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javahomes.springboot_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
