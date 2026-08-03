3f51eec15f91ccba314f546ab0693f7f0f44111d
Sonal Tamse <sonal.tamse@gmail.com>
2026-08-03 19:04:11 +0530

    (feat): create RabbitMQ consumer to consume json message

--- src/main/java/net/javahomes/springboot_rabbitmq/consumer/RabbitMQJsonConsumer.java
+++ src/main/java/net/javahomes/springboot_rabbitmq/consumer/RabbitMQJsonConsumer.java
@@ -1,4 +1,17 @@
 package net.javahomes.springboot_rabbitmq.consumer;
 
+import net.javahomes.springboot_rabbitmq.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RabbitMQJsonConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+
+    @RabbitListener(queues = "${rabbitmq.json.queue.name}")
+    public void consumeJsonMessage(User user) {
+        LOGGER.info("Received JSON message: {}", user);
+    }
 }
